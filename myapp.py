c52a9d23a307a1a2a8c5bc5dfe85e2d552a1c424
Marcio Ferreira Veiga <marciofv@hotmail.com>
2023-04-02 14:20:27 -0300

    add raw data

--- myapp.py
+++ myapp.py
@@ -1,2 +1,5 @@
 import streamlit as st
-st.write("Hello from Streamlit")
+st.title('Eddy Current')
+
+DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
+         'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
